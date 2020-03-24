20bbb753abc9c9ecf448c1dd29a4159406b549ad
Andrey Derkach <a.derkach@outlook.com>
2020-03-24 10:43:09 +0300

    Добавлен проект Utils в структуру

---
+++ Bochky.Utils/Logger/NLogLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NLog;
+using ILogger = Bochky.FindDirectory.Common.Interfaces.ILogger;
+
+namespace Bochky.Utils.Logger 
+{ 
+    public class NLogLogger : ILogger
+    {
+        private readonly NLog.Logger _logger;
+        public NLogLogger(string name)
+        {
+            _logger = LogManager.GetLogger(name);            
+        }
+
+        #region Implementation of ILogger
+
+        public void LogError(Exception ex)
+        {
+            LogError(ex, null);
+        }
+
+        public void LogError(Exception ex, string message)
+        {
+            if (message != null)
+                _logger.Log(LogLevel.Error, ex, message);
+            else
+                _logger.Log(LogLevel.Error, ex);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Log(LogLevel.Info, message);
+        }
+
+        public void LogDebug(string message)
+        {
+            _logger.Log(LogLevel.Debug, message);
+        }
+
+        #endregion
+    }
+}
